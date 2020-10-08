0548ff0fa07e67dd53f2489aef1fc0729f8533c1
Traphume <traphumedev@gmail.com>
2020-10-08 12:45:30 +0700

    Added placeholder sidebar component

---
+++ src/components/sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SideBar() {
+  return (
+    <nav>
+      <h1>Placeholder</h1>
+    </nav>
+  );
+}
+
+export default SideBar;
--- src/pages/root.js
+++ src/pages/root.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from "react-router-dom";
 
 import { AuthContext } from "../contexts/auth";
 import Login from "../components/login";
+import SideBar from "../components/sidebar";
 import Overview from "./overview";
 import Grades from "./grades";
 import Labs from "./labs";
@@ -14,20 +15,23 @@ function App() {
   }
 
   return (
-    <Switch>
-      <Route exact path="/">
-        <Redirect to="/overview" />
-      </Route>
-      <Route exact path="/overview">
-        <Overview />
-      </Route>
-      <Route exact path="/grades">
-        <Grades />
-      </Route>
-      <Route exact path="/labs">
-        <Labs />
-      </Route>
-    </Switch>
+    <>
+      <SideBar />
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/overview" />
+        </Route>
+        <Route exact path="/overview">
+          <Overview />
+        </Route>
+        <Route exact path="/grades">
+          <Grades />
+        </Route>
+        <Route exact path="/labs">
+          <Labs />
+        </Route>
+      </Switch>
+    </>
   );
 }
 

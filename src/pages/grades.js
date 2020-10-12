58a3018179329712211725dde6ba2abf3fd232d5
Traphume <traphumedev@gmail.com>
2020-10-12 18:15:04 +0700

    Refactor grades page to use cardlist component

--- src/pages/grades.js
+++ src/pages/grades.js
@@ -1,70 +1,18 @@
-import React, { useState, useEffect, useContext } from "react";
+import React from "react";
 
-import { AuthContext } from "../contexts/auth";
 import { fetchStudentGradeList } from "../repo/grade";
+import CardList from "../components/cardList";
 
-import styles from "./grades.module.css";
-
-function Grades({ current }) {
-  const {
-    state: { token },
-  } = useContext(AuthContext);
-
-  const [grades, setGrades] = useState([]);
-
-  useEffect(() => {
-    async function temp() {
-      try {
-        const arr = await fetchStudentGradeList(token, current);
-        setGrades(arr);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    temp();
-  }, [token, current]);
-
-  // Return nothing if have not selected subject
+function Grade({ current }) {
   if (current === "") {
     return <></>;
   }
 
   return (
     <>
-      <ul>
-        {grades.map((g) => {
-          return (
-            <li key={g.id} className={styles.card}>
-              <div className={styles.cardTop}>
-                <div>
-                  <h3 className={styles.name}>{g.name}</h3>
-                  <h6 className={styles.id}>{g.id}</h6>
-                </div>
-                <h5 className={styles.date}>{`${g.start}-${g.due}`}</h5>
-              </div>
-              <div className={styles.cardBottom}>
-                <h4
-                  className={
-                    g.type === "Quiz" ? styles.typeQuiz : styles.typeProg
-                  }
-                >
-                  {g.type}
-                </h4>
-                <h4
-                  className={
-                    g.status === "Pass" ? styles.statusPass : styles.statusFail
-                  }
-                >
-                  {g.status}
-                </h4>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+      <CardList current={current} api={fetchStudentGradeList} />
     </>
   );
 }
 
-export default Grades;
+export default Grade;

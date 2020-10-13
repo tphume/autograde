4bcff90d2a142407ee86ff5d403df7a9220d5cb2
Traphume <traphumedev@gmail.com>
2020-10-13 22:28:56 +0700

    Added call api on lab modal load. Added layout styling for lab modal

--- src/components/labModal.js
+++ src/components/labModal.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+
+import { fetchLabDetail } from "../repo/lab";
+import { AuthContext } from "../contexts/auth";
 
 import styles from "./labModal.module.css";
 
-function LabModal({ setdetail }) {
+function LabModal({ setdetail, detail }) {
+  const {
+    state: { token },
+  } = useContext(AuthContext);
+  const [state, setState] = useState({});
+
+  useEffect(() => {
+    async function temp() {
+      try {
+        const q = await fetchLabDetail(token, detail);
+        setState(q);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    temp();
+  }, [detail, token]);
+
   return (
     <section>
+      <h2 className={styles.title}>
+        There are {state.total} questions in total
+      </h2>
+      {state.type === "Quiz" ? Quiz(state) : Prog(state)}
       <div className={styles.footer}>
         <button className={styles.save}>SAVE</button>
         <button className={styles.exit} onClick={() => setdetail("")}>
@@ -14,5 +39,21 @@ function LabModal({ setdetail }) {
     </section>
   );
 }
+
+function Quiz(s) {
+  return (
+    <ul className={styles.questions}>
+      {s.questions.map((q, i) => {
+        return (
+          <li key={i} className={styles.item}>
+            <h2>{q.question}</h2>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function Prog() {}
 
 export default LabModal;

11ff5d8486d8076b65199aea345b6e15a07df348
Traphume <traphumedev@gmail.com>
2020-10-14 13:50:41 +0700

    Added separate mock for fetching

--- src/repo/overview.js
+++ src/repo/overview.js
@@ -1,6 +1,28 @@
 async function fetchOverview(token, subject) {
   //TODO: call api endpoint to get overview of a subject
   // this is currently a mock api call that will always succeed
+  if (subject === "") return { grades: {}, labs: {} };
+
+  if (subject === "100543432") {
+    return {
+      grades: {
+        total: 2,
+        pass: 2,
+        fail: 0,
+        quiz: 1,
+        prog: 1,
+      },
+      labs: {
+        total: 2,
+        pending: 1,
+        grading: 1,
+        late: 0,
+        quiz: 1,
+        prog: 1,
+      },
+    };
+  }
+
   return {
     grades: {
       total: 4,

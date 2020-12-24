ffa8873bb01e4a5375e71e726f0e831a6d0a7e04
Traphume <traphumedev@gmail.com>
2020-12-25 00:14:16 +0700

    Added form to register modal

--- src/components/registerModal.js
+++ src/components/registerModal.js
@@ -4,11 +4,74 @@ import { register } from "../repo/auth";
 import Modal from "./modal";
 
 import styles from "./registerModal.module.css";
+import loginStyles from "./login.module.css";
 
 function RegisterModal({ setShowReg }) {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <Modal>
-      <h1>Register Page</h1>
+      <section className={styles.root}>
+        <h1 className={loginStyles.title}>Create account</h1>
+        <h4 className={loginStyles.subTitle}>
+          Fill in all the filed to continue
+        </h4>
+        <form className={loginStyles.formContainer}>
+          <div className={loginStyles.inputContainer}>
+            <label className={loginStyles.inputLabel}>Name</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              minLength={3}
+              maxLength={30}
+              required
+              className={loginStyles.inputText}
+            ></input>
+          </div>
+          <div className={loginStyles.inputContainer}>
+            <label className={loginStyles.inputLabel}>Username</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              minLength={4}
+              maxLength={16}
+              required
+              className={loginStyles.inputText}
+            ></input>
+          </div>
+          <div className={loginStyles.inputContainer}>
+            <label className={loginStyles.inputLabel}>Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className={loginStyles.inputText}
+            ></input>
+          </div>
+          <div className={loginStyles.inputContainer}>
+            <label className={loginStyles.inputLabel}>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              minLength={4}
+              maxLength={16}
+              required
+              className={loginStyles.inputText}
+            ></input>
+          </div>
+          <input type="submit" value="Signup" className={loginStyles.login} />
+        </form>
+        <button onClick={() => setShowReg(false)} className={styles.exit}>
+          Cancel
+        </button>
+      </section>
     </Modal>
   );
 }

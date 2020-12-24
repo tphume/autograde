27662bc4fe6659e95e73236c00d96ab9479d2917
Traphume <traphumedev@gmail.com>
2020-12-24 23:17:06 +0700

    Added register api endpoint

--- src/repo/auth.js
+++ src/repo/auth.js
@@ -20,4 +20,31 @@ async function authenticate(username, password) {
     },
   };
 }
-export { authenticate };
+
+async function register(name, username, email, password) {
+  if (process.env.NODE_ENV === "production") {
+    const endpoint = process.env.REACT_APP_URL + "/api/auth/register/";
+
+    try {
+      const response = await axios.post(endpoint, {
+        name,
+        username,
+        email,
+        password,
+      });
+      return response.data;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  // Below is the mock api
+  return {
+    token: "faketoken",
+    user: {
+      id: 2,
+    },
+  };
+}
+
+export { authenticate, register };

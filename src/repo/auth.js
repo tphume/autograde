31b0ea6458275d1988b6f325b5e09ee7c9ed3869
Traphume <traphumedev@gmail.com>
2020-12-24 14:20:29 +0700

    Added axios login call

--- src/repo/auth.js
+++ src/repo/auth.js
@@ -1,7 +1,15 @@
+import axios from "axios";
+
 async function authenticate(username, password) {
   if (process.env.NODE_ENV === "production") {
-    //TODO: call api endpoint to authenticate user
-    return;
+    const endpoint = process.env.REACT_APP_URL + "/api/auth/login";
+
+    try {
+      const response = await axios.post(endpoint, { username, password });
+      return response.data;
+    } catch (e) {
+      throw e;
+    }
   }
 
   // Below is the mock api
